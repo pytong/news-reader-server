04c8668d9fbcddf1a5517a0972dbac40edd6cce2
Puiyee Tong <pytong07@gmail.com>
2018-06-15 01:05:02 -0400

    Add news API

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,14 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const cors = require('cors');
+const app = express();
+
+app.use(cors());
+
+const corsOptions = {
+  origin: 'http://localhost:3000',
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
--- routes/news.js
+++ routes/news.js
@@ -1,20 +1,30 @@
 const express = require('express');
+const cors = require('cors');
 const router = express.Router();
+const app = express();
 
 const axios = require('axios');
 const extractor = require('unfluff');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  axios.get('https://www.reuters.com/article/us-usa-salmonella-kellogg/kellogg-issues-massive-honey-smacks-cereal-recall-over-salmonella-risk-idUSKBN1JA32M')
-  .then(function (response) {
-    console.log(response.status)
+app.use(cors());
+
+var corsOptions = {
+  origin: 'http://localhost:3000',
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+
+router.get('/', async function(req, res, next) {
+  try {
+    const decodedUrl = decodeURI(req.query.articleUrl);
+    const response = await axios.get(decodedUrl);
+
     const htmlData = response.data;
-    console.log(extractor(htmlData).text);
-  })
-  .catch(function (error) {
-    console.log(error);
-  });
+    const newsContent = extractor(htmlData).text;
+
+    res.send({ content: newsContent })
+  } catch(e) {
+    console.log(e);
+  };
 });
 
 module.exports = router;
